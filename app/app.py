7da3446463176ed5dca293e685e7d23af545463a
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2020-11-22 16:15:26 -0300

    bd

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.secret_key = "secret"
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://root:pass@localhost/manageremploye'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://root:nbaka145236@localhost/manageremploye'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
-
+login_manager = LoginManager()
 class Employe(db.Model):
     idemploye = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -20,7 +22,7 @@ class Employe(db.Model):
 @app.route("/")
 def index():
     all_employes = Employe.query.all() 
-    return render_template("index.html", employes = all_employes)
+    return render_template("inicio.html") employes = all_employes)
 
 @app.route("/update", methods = ['GET', 'POST'])
 def update ():
