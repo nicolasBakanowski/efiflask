400ddc59845d64637bbab990c38905f783376d13
NaccN <n.campi@itecriocuarto.org.ar>
2020-11-24 14:20:38 -0300

    fotter y link de logo agregado

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = "secret"
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://root:nbaka145236@localhost/manageremploye'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://root:root@localhost/manageremploye'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
