b25ac207771f0a66c8bbed667ca66aa3b946ee90
NaccN <n.campi@itecriocuarto.org.ar>
2020-11-29 23:36:27 -0300

    Esqueleto login

--- app/app.py
+++ app/app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, DataRequired
+from models import users, get_user
+from werkzeug.urls import url_parse
 
 app = Flask(__name__)
 app.secret_key = "secret"
 app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://root:root@localhost/manageremploye'
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 db = SQLAlchemy(app)
-login_manager = LoginManager()
+
 
 class Employe(db.Model):
     idemploye = db.Column(db.Integer, primary_key=True)
@@ -20,12 +29,55 @@ class Employe(db.Model):
         self.email = email
         self.phone = phone 
 
+
+@login_manager.user_loader 
+def load_user(user_id):
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Recuérdame')
+    submit = SubmitField('Login')
+
 @app.route("/")
 def index():
     all_employes = Employe.query.all() 
     return render_template("index.html",employes = all_employes)
 
+# elementos signup
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    return render_template("signup_form.html")
+# fin elementos signup
+
+# elementos Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:  # Si el usuario ya está autenticado no tiene sentido que se vuelva a loguear, por lo que lo redirigimos a la página principal.
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit(): 
+        user = get_user(form.email.data) #si los datos enviados en el formulario son válidos. En ese caso, intentamos recuperar el usuario a partir del email con get_user()
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data) #Si existe un usuario con dicho email y la contraseña coincide, procedemos a autenticar al usuario llamando al método login_user
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+    return render_template('login_form.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+# fin elementos Login
+
 @app.route("/update", methods = ['GET', 'POST'])
+@login_required
 def update ():
     if request.method == "POST":
         employe = Employe.query.get(request.form.get('idemploye'))
@@ -36,7 +88,8 @@ def update ():
         flash("Actualizado correctamente")
         return redirect(url_for('index'))
 
-@app.route("/insert", methods = ['POST']) 
+@app.route("/insert", methods = ['POST'])
+@login_required 
 def insert():
     if request.method == "POST":
         name = request.form['name']
@@ -49,6 +102,7 @@ def insert():
         return redirect(url_for('index'))
 
 @app.route("/delete/<id>/",methods= ["GET","POST"])
+@login_required 
 def delete(id):
     employe = Employe.query.get(id)
     db.session.delete(employe)
@@ -56,5 +110,4 @@ def delete(id):
     flash("El trabajador fue borrado con éxito")
     return redirect(url_for("index"))
 
-if __name__== "__main__":
-    app.run(debug=True) 
+app.run(debug=True) 
